1e597c89bcd2adc8a43ce10771c6fe2391aa15a8
KwSalem <167813808+KwSalem@users.noreply.github.com>
2024-11-21 13:30:26 +0300

    done task

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import Input from "./Input";
+import { useMutation } from "@tanstack/react-query";
+import { addPet } from "../API/pets";
 
 const Modal = ({ show, setShowModal }) => {
   const [name, setName] = useState("");
   const [type, setType] = useState("");
   const [image, setImage] = useState("");
   const [available, setAvailable] = useState(0);
+  const mutation = useMutation({
+    mutationKey: ["addPet"],
+    mutationFn: (newPetData) => addPet(newPetData),
+  });
+  const handleSubmit = () => {
+    mutation.mutate({
+      name: name,
+      type: type,
+      image: image,
+      adopt: available,
+    });
+    if (mutation.isSuccess) {
+      setShowModal(false);
+    }
+  };
+
   if (!show) return "";
+
   return (
     <div
       className="inset-0 fixed  flex justify-center items-center flex-col z-20 overflow-hidden 
@@ -47,7 +66,10 @@ const Modal = ({ show, setShowModal }) => {
           }}
         />
 
-        <button className="w-[70px] border border-black rounded-md ml-auto mr-5 hover:bg-green-400">
+        <button
+          onClick={handleSubmit}
+          className="w-[70px] border border-black rounded-md ml-auto mr-5 hover:bg-green-400"
+        >
           Submit
         </button>
       </div>
